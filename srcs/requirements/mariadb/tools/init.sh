3a086949770883f6a4c7390a6ffbb3badff317dc
fakoukou <fakoukou@incep.myguest.virtualbox.org>
2026-04-30 17:35:33 +0100

    inc

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 5
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+
+mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+service mariadb stop
+
+exec mysqld_safe
