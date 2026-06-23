1ad7fabc9e8b2a5fbb8c39f3d97adc8f3708dbef
fakoukou <fakoukou@incep.myguest.virtualbox.org>
2026-06-23 12:49:49 +0100

    final

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -5,18 +5,23 @@ set -e
 echo "[mariadb] starting..."
 
 mysqld_safe --datadir=/var/lib/mysql &
-sleep 5
+MYSQL_PID=$!
 
+# wait DB ready
 until mysqladmin ping -h 127.0.0.1 --silent; do
     echo "Waiting for MariaDB..."
     sleep 1
 done
 
+echo "[mariadb] creating database and user..."
+
 mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-wait
+echo "[mariadb] done"
+
+wait $MYSQL_PID
