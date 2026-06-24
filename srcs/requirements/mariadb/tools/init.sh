d6c56a831b6e4671f06b2541909b81ae3cdcc7be
fakoukou <fakoukou@incep.myguest.virtualbox.org>
2026-06-24 14:32:42 +0100

    fd

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
 set -e
 
 echo "[mariadb] starting..."
-
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
 mysqld_safe --datadir=/var/lib/mysql &
 MYSQL_PID=$!
 
-
 until mysqladmin ping -h 127.0.0.1 --silent; do
-    echo "Waiting for MariaDB..."
+    echo "Waiting MariaDB..."
     sleep 1
 done
 
-echo "[mariadb] creating database and user..."
+echo "[mariadb] creating db..."
 
 mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
@@ -22,6 +22,6 @@ GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-echo "[mariadb] done"
+echo "[mariadb] ready"
 
 wait $MYSQL_PID
